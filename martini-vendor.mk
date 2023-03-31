8ad90b19531c5b89785813d65b54eee41b831b63
Georg Veichtlbauer <georg@vware.at>
2023-03-31 19:18:54 +0530

    martini: Remove aptX(HD) encoder shared libs
    Encoder shared libraries are no longer required as of Android 13 QPR2.
    
    Change-Id: I893a9000070cb2582ada0af91547f5176e6fea84

--- martini-vendor.mk
+++ martini-vendor.mk
@@ -2423,8 +2423,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libfastcvopt \
     libthermalclient \
-    libaptXHD_encoder \
-    libaptX_encoder \
     CACertService \
     CneApp \
     IWlanService \
